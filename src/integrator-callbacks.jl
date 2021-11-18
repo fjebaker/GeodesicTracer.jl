67fed28287f01a62c376198b82369adad6a07476
fjebaker <fergusbkr@gmail.com>
2021-11-18 16:47:43 +0000

    Carter: dead pixel fix
    # Conflicts:
    #	src/integrator-callbacks.jl

--- src/integrator-callbacks.jl
+++ src/integrator-callbacks.jl
@@ -4,10 +4,8 @@
 In practice, we check ``r > R_0 + \\epsilon``, with ``\\epsilon`` small, and ``r < \\infty``, 
 where our effective infinity is hardcoded as `1200.0`.
 """
-@inline function chartbounds(u, λ, integrator)
-    p = integrator.p
-
-    on_chart = p.chart_inner_radius < u[6] < 1200.0
+@inline function chartbounds(r, p)
+    on_chart = p.chart_inner_radius < r < 1200.0
     !on_chart
 end
 
@@ -18,7 +16,7 @@ Monitors [`Vr`](@ref) and [`Vθ`](@ref) and flips signs stored repsectively in t
 integrator parameter if they are negative, along with storing the time at which this 
 occured.
 """
-function signflip(u, λ, integrator)
+@inline function signflip(u, λ, integrator)
     p = integrator.p
     metric = p.metric
 
@@ -27,9 +25,26 @@ function signflip(u, λ, integrator)
     elseif Vθ(metric.E, p.L, p.Q, metric.a, u[3]) < 0
         integrator.p = flip_θsign(λ, p)
     end
-
-    chartbounds(u, λ, integrator)
 end
+
+@inline function is_radial_pot_negative(u, λ, integrator)
+    p = integrator.p
+    Vr(p.metric.E, p.L, p.metric.M, p.Q, u[2], p.metric.a) < 0
+end
+
+@inline function is_angular_pot_negative(u, λ, integrator)
+    p = integrator.p
+    Vθ(p.metric.E, p.L, p.Q, p.metric.a, u[3]) < 0
+end
+
+@inline function flip_radial_sign(integrator)
+    integrator.p = flip_rsign(integrator.t, integrator.p)
+end
+
+@inline function flip_angular_sign(integrator)
+    integrator.p = flip_θsign(integrator.t, integrator.p)
+end
+
 
 """
     $(TYPEDSIGNATURES)
@@ -39,18 +54,33 @@ the disk given by `disk`.
 """
 function wrapcallback(s::BHSetup, disk)
     cb =
-        isnothing(disk) ? chartbounds :
+        isnothing(disk) ? (u, λ, integrator) -> chartbounds(u[6], integrator.p) :
         (u, λ, integrator) -> begin
-            chartbounds(u, λ, integrator) || intersect!(integrator, u, disk)
+            # indexing for 2nd order ODE problem has velocity in 1:4, position 5:8
+            chartbounds(u[6], integrator.p) || intersect!(integrator, @view(u[5:8]), disk)
         end
+    #CallbackSet(
     DiscreteCallback(cb, terminate!)
+    #DiscreteCallback(
+    #    (u, λ, integrator) -> u[6] < 5 * integrator.p.chart_inner_radius,
+    #    adjust_time_step
+    #    )
+    #)
 end
 
 function wrapcallback(s::BHSetup{CarterBoyerLindquist{T}}, disk) where {T}
-    cb =
-        isnothing(disk) ? signflip :
+    chart_callback =
+        isnothing(disk) ? (u, λ, integrator) -> begin 
+            signflip(u, λ, integrator)
+            chartbounds(u[2], integrator.p)
+        end :
         (u, λ, integrator) -> begin
-            signflip(u, λ, integrator) || intersect!(integrator, u, disk)
+            signflip(u, λ, integrator)
+            chartbounds(u[2], integrator.p) || intersect!(integrator, u, disk)
         end
-    DiscreteCallback(cb, terminate!)
+    CallbackSet(
+        DiscreteCallback(is_radial_pot_negative, flip_radial_sign),
+        DiscreteCallback(is_angular_pot_negative, flip_angular_sign),
+        DiscreteCallback(chart_callback, terminate!)
+    )
 end
