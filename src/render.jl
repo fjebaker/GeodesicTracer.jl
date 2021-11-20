0f37b1df4392785867fa2243b5d4882b1cc62618
fjebaker <fergusbkr@gmail.com>
2021-11-20 16:17:43 +0000

    allow solvers to be passed to render functions also

--- src/render.jl
+++ src/render.jl
@@ -1,8 +1,8 @@
 """
     $(TYPEDSIGNATURES)
 """
-@inline function render!(row, α_range, β, d, s::BHSetup, valuefunc::ValueFunction)
-    simsols = calcgeodesic(α_range, s.img_width, β, s, disk = d, save_geodesics = false)
+@inline function render!(row, α_range, β, d, s::BHSetup, valuefunc::ValueFunction; solver=Tsit5())
+    simsols = calcgeodesic(α_range, s.img_width, β, s, disk = d, save_geodesics = false, solver=solver)
     Threads.@threads for i = 1:s.img_width
         row[i] = valuefunc(simsols[i], s, d)
     end
@@ -20,11 +20,11 @@ the redshift if it intercepts the disk.
 
 Allocates and returns a 2-dimensional `Float64` array.
 """
-function render(d, s::BHSetup; valuefunc = geometry)
+function render(d, s::BHSetup; valuefunc = geometry, kwargs...)
     # preallocate image output
     image = zeros(Float64, (s.img_height, s.img_width))
 
-    render!(image, d, s, valuefunc)
+    render!(image, d, s, valuefunc; kwargs...)
     image
 end
 render(s::BHSetup; kwargs...) = render(nothing, s; kwargs...)
@@ -35,7 +35,7 @@ render(s::BHSetup; kwargs...) = render(nothing, s; kwargs...)
 
 Inplace version of [`renderdisk`](@ref), where the image is allocated by the user.
 """
-function render!(image, d, s::BHSetup, valuefunc::ValueFunction)
+function render!(image, d, s::BHSetup, valuefunc::ValueFunction; kwargs...)
     y_mid = s.img_height ÷ 2
     x_mid = s.img_width ÷ 2
 
@@ -43,7 +43,7 @@ function render!(image, d, s::BHSetup, valuefunc::ValueFunction)
         # generate space of α to integrate
         # have to use a slight 0.01 offset to avoid integrating α=0.0 geodesics
         α_range = ((0.01 - x_mid) / s.fov_factor, (0.01 + x_mid) / s.fov_factor)
-        render!(@view(image[Y, :]), α_range, (Y - y_mid) / s.fov_factor, d, s, valuefunc)
+        render!(@view(image[Y, :]), α_range, (Y - y_mid) / s.fov_factor, d, s, valuefunc; kwargs...)
     end
 end
 
